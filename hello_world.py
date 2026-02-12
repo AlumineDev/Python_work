651337ce76c60b9f08606e3f9ae5b76b303acf85
AlumineDev <AlumineDev@hotmail.com>
2026-02-12 12:59:41 -0700

    Ejercicio 1: Modificar variables en el código de Python

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,12 @@
 print("Hello, World!")
+mensaje = "¡Hola, Mundo!"
+print(mensaje)
+mensaje = "¡Hola, Python!"
+print(mensaje)
+nombre = "Alice"
+print("Hola, " + nombre + "!")
+edad = 30
+print("Tengo " + str(edad) + " años.")
+edad = 37
+print("Ahora tengo " + str(edad) + " años.")
+
